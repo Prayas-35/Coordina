62666caed647c3ee905698c07bf16a27a20d3cc0
Kaniska <kaniska.mitra@gmail.com>
2024-09-05 03:37:36 +0530

    Team Page

---
+++ main/app/team/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className='bg-background'> 
+      <h1 className='text-foreground'>Team</h1>
+    </div>
+  )
+}
+
+export default Landing
