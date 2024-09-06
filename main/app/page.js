c623d227d7af9af8e8ec823b80d93481c9a49c36
Prayas <prayaspal04@gmail.com>
2024-09-06 17:38:56 +0530

    landing

--- main/app/page.js
+++ main/app/page.js
@@ -1,13 +1,36 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 
 function Landing() {
   return (
-    <div className='bg-background'> 
-      <h1 className='text-foreground'>Landing Page</h1>
+    <div className="flex flex-col min-h-screen">
+      <HeroHighlight>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto ">
+          With insomnia, nothing&apos;s real. Everything is far away. Everything
+          is a{" "}
+          <Highlight className="text-black dark:text-white">
+            copy, of a copy, of a copy.
+          </Highlight>
+        </motion.h1>
+      </HeroHighlight>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
