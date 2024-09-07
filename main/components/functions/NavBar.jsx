c7f0800dfd6b0832db1d6596b427fb5561ff8bc6
Prayas <prayaspal04@gmail.com>
2024-09-08 00:48:07 +0530

    favicon and navbar

--- main/components/functions/NavBar.jsx
+++ main/components/functions/NavBar.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import { ModeToggle } from "../theme/ThemeSwitcher"
-import { useRouter } from "next/navigation"
+// import { useRouter } from "next/navigation"
 import { motion, AnimatePresence } from "framer-motion"
 import { GrResources } from "react-icons/gr";
 import { FaCompressArrowsAlt } from "react-icons/fa";
@@ -208,25 +208,6 @@ function HomeIcon(props) {
         >
             <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
             <polyline points="9 22 9 12 15 12 15 22" />
-        </svg>
-    )
-}
-
-function MountainIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
         </svg>
     )
 }
