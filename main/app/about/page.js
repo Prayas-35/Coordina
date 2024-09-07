980a146cdd9bfdc22b3dbcd3f9e41808b57b3f69
Swikriti Mukherjee <swikritim05@gmail.com>
2024-09-08 02:40:12 +0530

    About Page Updated

--- main/app/about/page.js
+++ main/app/about/page.js
@@ -1,34 +1,38 @@
 "use client";
 
 import React from 'react';
+import Navbar from '@/components/functions/NavBar';
 
 function Landing() {
   const teamMembers = [
-    { name: 'Prayas Pal', role: 'Team Lead', imgSrc: 'Prayas.jpg' },
-    { name: 'Swikriti Mukherjee', role: 'UI/UX Designer', imgSrc: 'Swikriti.jpg' },
-    { name: 'Kaniska Mitra', role: 'Data Scientist', imgSrc: 'Kaniska.jpg' },
-    { name: 'Sudarshan Chaudhuri', role: 'Backend Developer', imgSrc: 'Sudarshan.jpeg' },
-    { name: 'Swajan Khasnobis', role: 'DevOps Engineer', imgSrc: 'Swajan.jpg' },
-    { name: 'Sabittwa Banerjee', role: 'Frontend Developer', imgSrc: 'Sabittwa.jpg' },
+    { name: 'Sabittwa Banerjee', role: '', imgSrc: 'Sabittwa.jpg' },
+    { name: 'Prayas Pal', role: '(Team Lead)', imgSrc: 'Prayas.jpg' },
+    { name: 'Swikriti Mukherjee', role: '', imgSrc: 'Swikriti.jpg' },
+    { name: 'Sudarshan Chaudhuri', role: '', imgSrc: 'Sudarshan.jpeg' },
+    { name: 'Kaniska Mitra', role: '', imgSrc: 'Kaniska.jpg' },
+    { name: 'Swajan Khasnobis', role: '', imgSrc: 'Swajan.jpg' },
   ];
 
   return (
-    <div className="bg-background min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-foreground mb-8">About Our Team</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="bg-foreground rounded-lg shadow-lg p-6 flex flex-col items-center">
-            <img
-              src={member.imgSrc}
-              alt={member.name}
-              className="w-32 h-32 object-cover rounded-full mb-4"
-            />
-            <h2 className="text-xl font-semibold text-background">{member.name}</h2>
-            <p className="text-background">{member.role}</p>
-          </div>
-        ))}
+    <>
+      <Navbar />
+      <div className="bg-background min-h-[100vh-72.8] flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-foreground mb-5 pt-5">About Our Team</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
+          {teamMembers.map((member, index) => (
+            <div key={index} className="bg-transparent rounded-lg shadow-lg p-6 flex flex-col items-center border-4 border-border">
+              <img
+                src={member.imgSrc}
+                alt={member.name}
+                className="w-32 h-32 object-cover rounded-full mb-4 border-4 border-border"
+              />
+              <h2 className="text-xl font-semibold text-primary">{member.name}</h2>
+              <p className="text-chart-2 pt-1">{member.role}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
