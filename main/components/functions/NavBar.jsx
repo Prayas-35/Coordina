28a2ba174b7959659a8d035d0da292a178fe3be5
Prayas <prayaspal04@gmail.com>
2024-09-15 22:21:10 +0530

    show uid

--- main/components/functions/NavBar.jsx
+++ main/components/functions/NavBar.jsx
@@ -1,53 +1,68 @@
 'use client';
 
-import { useState, useEffect } from "react"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu"
-import { ModeToggle } from "../theme/ThemeSwitcher"
-import { useRouter } from "next/navigation"
-import { motion, AnimatePresence } from "framer-motion"
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "../theme/ThemeSwitcher";
+import { useRouter } from "next/navigation";
+import { motion, AnimatePresence } from "framer-motion";
 import { GrResources } from "react-icons/gr";
 import { FaCompressArrowsAlt } from "react-icons/fa";
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 import { GoDiscussionClosed } from "react-icons/go";
 import { useAuth } from "@/app/_contexts/authcontext";
+import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogClose } from '@/components/ui/dialog';
 
 export default function Navbar() {
-    const router = useRouter()
-    const { token, logout } = useAuth()
-    const [name, setName] = useState('')
-    const [uid, setUid] = useState('')
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const { resolvedTheme } = useTheme()
-      const getName = async () => {
-        const res = await fetch("/api/getName", {
-          method: "GET",
-          headers: {
-            Authorization: token
-          }
-        })
-        const data = await res.json()
-        console.log(data)
-        setUid(data.uid)
-        setName(data.name)
-        console.log(name)
-      }
+    const router = useRouter();
+    const { token, logout } = useAuth();
+    const [name, setName] = useState('');
+    const [uid, setUid] = useState('');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [showUidDialog, setShowUidDialog] = useState(false); // State for dialog visibility
+    const { resolvedTheme } = useTheme();
+    const [copy, setCopy] = useState("Copy");
 
-      useEffect(() => {
+    const getName = async () => {
+        const res = await fetch("/api/getName", {
+            method: "GET",
+            headers: {
+                Authorization: token
+            }
+        });
+        const data = await res.json();
+        setUid(data.uid);
+        setName(data.name);
+    };
+
+    useEffect(() => {
         if (token) {
-          getName()
+            getName();
         }
-      }, [token])
+    }, [token]);
 
     const handleLogout = () => {
-        logout()
-        router.push("/")
-    }
+        logout();
+        router.push("/");
+    };
 
     const handleMenuToggle = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleShowUid = () => {
+        setShowUidDialog(true);
+    };
+
+    const handleCopyUid = () => {
+        navigator.clipboard.writeText(uid);
+        setCopy("Copied!");
+        setTimeout(() => {
+            setCopy("Copy");
+        }, 2000);
+        // alert('UID copied to clipboard!');
+    };
 
     return (
         <>
@@ -84,7 +99,7 @@ export default function Navbar() {
                             </div>
                         </nav>
                         <ModeToggle />
-                        <UserMenu name={name} handleLogout={handleLogout} />
+                        <UserMenu name={name} handleLogout={handleLogout} handleShowUid={handleShowUid} />
                     </div>
                 </div>
             </header>
@@ -96,7 +111,7 @@ export default function Navbar() {
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 0.3 }}
-                            className="fixed inset-0 bg-black/50 z-40 lg:hidden"
+                            className="fixed inset-0 bg-black/50 backdrop-blur-md z-40 lg:hidden"
                             onClick={handleMenuToggle}
                         />
                         <motion.nav
@@ -104,7 +119,7 @@ export default function Navbar() {
                             animate={{ opacity: 1, y: 0 }}
                             exit={{ opacity: 0, y: -20 }}
                             transition={{ duration: 0.3 }}
-                            className="fixed top-14 left-0 right-0 z-50 bg-background/70 backdrop-blur-lg px-4 py-6 lg:hidden"
+                            className="fixed top-14 left-0 right-0 z-50 bg-background/70 backdrop-blur-md px-4 py-6 lg:hidden"
                         >
                             <div className="grid gap-4">
                                 <NavLink href="/dashboard" icon={<HomeIcon className="h-4 w-4" />} text="Dashboard" />
@@ -116,8 +131,42 @@ export default function Navbar() {
                     </>
                 )}
             </AnimatePresence>
+            
+            <Dialog open={showUidDialog} onOpenChange={setShowUidDialog}>
+                <DialogTrigger />
+                <AnimatePresence>
+                    {showUidDialog && (
+                        <>
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.3 }}
+                            />
+                            <motion.div
+                                initial={{ opacity: 0, scale: 0.8 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.8 }}
+                                transition={{ duration: 0.3 }}
+                                className="fixed inset-0 flex items-center justify-center z-50"
+                            >
+                                <DialogContent className=" p-6 rounded-lg shadow-lg">
+                                    <DialogTitle className="text-lg font-semibold mb-4">Your UID</DialogTitle>
+                                    <div className="flex justify-between items-center gap-2">
+                                        <span className="text-gray-800 dark:text-gray-200">{uid}</span>
+                                        <Button onClick={handleCopyUid}>{copy}</Button>
+                                    </div>
+                                    <DialogClose asChild>
+                                        <Button className="mt-4">Close</Button>
+                                    </DialogClose>
+                                </DialogContent>
+                            </motion.div>
+                        </>
+                    )}
+                </AnimatePresence>
+            </Dialog>
         </>
-    )
+    );
 }
 
 function NavLink({ href, icon, text }) {
@@ -130,10 +179,10 @@ function NavLink({ href, icon, text }) {
             {icon}
             {text}
         </Link>
-    )
+    );
 }
 
-function UserMenu({ name, handleLogout }) {
+function UserMenu({ name, handleLogout, handleShowUid }) {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -154,12 +203,10 @@ function UserMenu({ name, handleLogout }) {
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel>{name}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <Link href="/profile">
-                    <DropdownMenuItem>
-                        <UserIcon className="mr-2 h-4 w-4" />
-                        <span>Profile</span>
-                    </DropdownMenuItem>
-                </Link>
+                <DropdownMenuItem onClick={handleShowUid}>
+                    <UserIcon className="mr-2 h-4 w-4" />
+                    <span>Show UID</span>
+                </DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={handleLogout}>
                     <LogOutIcon className="mr-2 h-4 w-4" />
@@ -167,7 +214,7 @@ function UserMenu({ name, handleLogout }) {
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
-    )
+    );
 }
 
 function MenuIcon(props) {
@@ -184,11 +231,11 @@ function MenuIcon(props) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <line x1="4" x2="20" y1="12" y2="12" />
             <line x1="4" x2="20" y1="6" y2="6" />
+            <line x1="4" x2="20" y1="12" y2="12" />
             <line x1="4" x2="20" y1="18" y2="18" />
         </svg>
-    )
+    );
 }
 
 function HomeIcon(props) {
@@ -205,10 +252,9 @@ function HomeIcon(props) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-            <polyline points="9 22 9 12 15 12 15 22" />
+            <path d="M3 12l9-9 9 9v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V12z" />
         </svg>
-    )
+    );
 }
 
 function UserIcon(props) {
@@ -225,51 +271,10 @@ function UserIcon(props) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-            <circle cx="12" cy="7" r="4" />
+            <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4z" />
+            <path d="M4 20v-2a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v2" />
         </svg>
-    )
-}
-
-function SettingsIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-            <circle cx="12" cy="12" r="3" />
-        </svg>
-    )
-}
-
-function HelpCircleIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-            <path d="M12 17h.01" />
-        </svg>
-    )
+    );
 }
 
 function LogOutIcon(props) {
@@ -286,9 +291,8 @@ function LogOutIcon(props) {
             strokeLinecap="round"
             strokeLinejoin="round"
         >
-            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-            <polyline points="16 17 21 12 16 7" />
-            <line x1="21" x2="9" y1="12" y2="12" />
+            <path d="M10 17l5-5-5-5" />
+            <path d="M15 12H3" />
         </svg>
-    )
+    );
 }
