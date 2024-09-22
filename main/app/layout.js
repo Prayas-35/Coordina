6264c8afbf1fbcbad00dc8f8c8fa4764674fbb04
Prayas <prayaspal04@gmail.com>
2024-09-22 19:49:44 +0530

    font fix again!

--- main/app/layout.js
+++ main/app/layout.js
@@ -10,6 +10,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Object+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <AuthProvider>
           <ThemeProvider
